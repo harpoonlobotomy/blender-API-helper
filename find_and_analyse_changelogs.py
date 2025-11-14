0585790dd72536e8be4477d8f8ebcd580405dab7
Gabriel Lee <harpoonlobotomy@gmail.com>
2025-11-14 16:45:29 +0000

    Ignore this one.
    Just committing this so I can come back to it later and figure out why it failed and how to fix it. For now just doing an alternative instead.

--- find_and_analyse_changelogs.py
+++ find_and_analyse_changelogs.py
@@ -86,6 +86,9 @@ def get_initial_files(source_v, target_v):
     print(f"local logs: {local_logs}")
     return link_dict, local_logs
 
+def breakdown_blocks(blocks):
+    pass
+
 def parse_changelog(filepath_out):
     #doing this with line by line parsing, because docutils has piss-poor documentation and Sphinx is heavy and I'm too tired. Wasted a day on them already.
     print("Pretend I'm parsing things.")
@@ -95,6 +98,7 @@ def parse_changelog(filepath_out):
             line_list.append(line.rstrip())
     block_starts=[] #better way of doing this, this was from a previous trial and now just functions as a test bool for the first run.
     blocks={}
+    blocks_detailed={}
     counter=0
     active_block=False
     block_type=None
@@ -105,13 +109,25 @@ def parse_changelog(filepath_out):
             if active_block:
                 start=blocks[counter-1]["block_starts"]
                 lines_in_block=[]
-                for block_i, lines in enumerate(line_list):  # should make this a function.
-                    if start<block_i<i:
-                        if lines == '':
+                detailed_lines_in_block=[]
+                for block_i, line in enumerate(line_list):  #### !! this only appends the first and last entry. Changing how I do it.
+                    if start<block_i<i: # should this be 'while' instead of 'if'?
+                        if line == '':
                             continue
                         if not (line.startswith("----") or i+1 == len(line_list)): # makes sure it appends the last entry
                             continue
-                        lines_in_block.append(lines) # not sure if I want to do this and then finesse the results separately or just do it directly. Really should do it directly if there's no benefit to double handling.
+                        if line.startswith("^^^"):
+                            if line_list[block_i+1] in ("Added", "Renamed", "Replaced", "Removed", "Function Arguments"):
+                                blocks_detailed[counter-1].update({block_type: {"block_contents": detailed_lines_in_block}})
+                                detailed_lines_in_block=[]
+                            continue
+                            # here, we should get the next line, and append the old block_type and block so far to a dict.
+                        if line.strip() in ("Added", "Renamed", "Replaced", "Function Arguments", "Removed"):
+                            block_type=line.strip()
+                            continue
+                        #lines_in_block == all lines, inc headers, ^^^, etc.
+                        detailed_lines_in_block.append(line)
+                        lines_in_block.append(line) # not sure if I want to do this and then finesse the results separately or just do it directly. Really should do it directly if there's no benefit to double handling.
                         #while line.startswith("*"): <- something like this?
                         #if lines.startswith("^^^^"):
                         #    blocks[counter-1].update({"block_ends": i-1, block_type: {"block_contents": lines_in_block}})
@@ -120,9 +136,11 @@ def parse_changelog(filepath_out):
                         #    #blocks[counter-1].update({"block_type": {line_list[block_i+1]:None}})
                         #    continue
                         blocks[counter-1].update({"block_ends": i, "block_contents": lines_in_block})
+                        blocks_detailed[counter-1].update({"block_ends": i})
             if not line.startswith("----"):
                 continue
             blocks[counter]={"block_starts": i+1, "block_item": line_list[i+1]}
+            blocks_detailed[counter]={"block_item": line_list[i+1]}
             block_starts.append(i+1)
         #    affected=[]
             active_block=True
@@ -134,7 +152,8 @@ def parse_changelog(filepath_out):
         #    affected.append(line) # needs characters stripped, and :class:.
         #    print(f"counter: {counter}")
         #    blocks[counter]["change_type"].get(change_type).update(affected)#blocks[counter].update({"affected"})
-    print(f"blocks: {blocks}")
+    #print(f"blocks: {blocks_detailed}")
+    breakdown_blocks(blocks)
 
 def make_changelog(local_logs, source_v, target_v):
     #cmd_line = r'python D:/Git_Repos/blender-API-helper/sphinx_changelog_gen.py -- --indexpath="D:/Git_Repos/blender-API-helper/api_dumps_rst_output/api_dump_index.json" changelog --filepath-in-from D:/Git_Repos/blender-API-helper/api_dumps_rst_output/3.1.json --filepath-in-to D:/Git_Repos/blender-API-helper/api_dumps_rst_output/4.5.json --filepath-out changes.rst'
